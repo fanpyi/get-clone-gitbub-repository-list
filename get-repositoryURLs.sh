d622ac1724e03d01b177f2123aa45e8d68221498
fanpyi <fanpyi@163.com>
2016-07-06 11:52:43 +0800

    update reps

--- get-repositoryURLs.sh
+++ get-repositoryURLs.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 currentPath=$PWD 
-repURLsFileName='repositorys.txt'
+repURLsPath=${currentPath}/'repositorys.txt'
+rm -rf $repURLsPath
 cd $HOME/Desktop/gclone
 i=0
 for file in $(ls); do
 	let i++
 	cd $file 
-	git remote -v | grep 'fetch' | awk '{print $2}' >> ${currentPath}/${repURLsFileName}
+	git remote -v | grep 'fetch' | awk '{print $2}' >> $repURLsPath
  	cd ../
 done
-echo "Total: ${i}" >>${currentPath}/${repURLsFileName}
+echo "Total: ${i}" >>$repURLsPath
